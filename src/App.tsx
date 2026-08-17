a6b1369e7636330743a9331eb0c73600a42d9a09
headshoot17 <djenmarlet@gmail.com>
2026-08-17 14:04:40 +0100

    fix: add fallback background color for iOS Safari compatibility

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,15 @@ import { LoginPage } from "./components/LoginPage";
 
 function App() {
   return (
-    <div className="min-h-screen bg-pf-petrol-1 dark:bg-pf-petrol-11 flex flex-col font-grostek">
+    <div className="min-h-screen bg-pf-petrol-1 dark:bg-pf-petrol-11 flex flex-col font-grostek" style={{ backgroundColor: "#eef6f6" }}>
       {/* Header */}
       <Header />
 
       {/* Spacer below header */}
-      <div className="h-12 bg-pf-petrol-1 dark:bg-pf-petrol-11 shrink-0" />
+      <div className="h-12 bg-pf-petrol-1 dark:bg-pf-petrol-11 shrink-0" style={{ backgroundColor: "#eef6f6" }} />
 
       {/* Main content area */}
-      <div className="flex-grow bg-pf-petrol-1 dark:bg-pf-petrol-11 px-2 sm:px-5 pt-5 pb-32">
+      <div className="flex-grow bg-pf-petrol-1 dark:bg-pf-petrol-11 px-2 sm:px-5 pt-5 pb-32" style={{ backgroundColor: "#eef6f6" }}>
         <main className="flex flex-col flex-1">
           <LoginPage />
         </main>
